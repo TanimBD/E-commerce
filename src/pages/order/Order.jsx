4978fd67f9039b2e0cfab20cb8225b9b0ca21560
SabbirMeazee <mdsabbirmeazee69@gmail.com>
2023-12-12 17:55:59 +0600

    Navbar added

--- src/pages/order/Order.jsx
+++ src/pages/order/Order.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Layout from '../../components/layout/Layout'
+import myContext from '../../context/data/myContext'
 
 function Order() {
+    const context = useContext(myContext);
+    const { name, roll } = context;
     return (
         <div>
-            <Layout>Order</Layout>
+            <Layout>
+                <h1>Name: {name}</h1>
+                <h1>Roll: {roll}</h1>
+            </Layout>
         </div>
     )
 }
