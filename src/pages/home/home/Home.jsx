82758ddbc001df378b19e7ac4de85665a65b2a73
TanimBD <tanimhossen6@gmail.com>
2023-12-12 22:54:30 +0600

    Update Home.jsx

--- src/pages/home/home/Home.jsx
+++ src/pages/home/home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Layout from '../../../components/layout/Layout'
 import HeroSection from '../../../components/HeroSection/herosection'
+import Filter from '../../../components/filter/filter'
 
 function Home() {
     return (
         <Layout>sabbir
             <HeroSection/>
+            <Filter/>
         </Layout>
         
 
