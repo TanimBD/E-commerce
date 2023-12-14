e88d8f47347044f6bf17ac053cf3cd0124a68edc
Emu <emuchow50@gmail.com>
2023-12-15 00:31:04 +0600

    resultant

---
+++ src/pages/cart/Emu/kocks.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function () {
+  return (
+    <div>
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+    </div>
+  )
+}
+
+export default 
