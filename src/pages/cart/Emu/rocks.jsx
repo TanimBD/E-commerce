dd4a544d6cb8fbe6a8bd3a09f7b34b59c49ff2f5
Emu <emuchow50@gmail.com>
2023-12-15 00:28:39 +0600

    imag

---
+++ src/pages/cart/Emu/rocks.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function () {
+  return (
+    <div>
+
+        import React from 'react'
+import Layout from '../../components/layout/Layout'
+
+function Home() {
+  return (
+    <Layout>Home</Layout>
+  )
+}
+
+export default Home
+    </div>
+  )
+}
+
+export default 
